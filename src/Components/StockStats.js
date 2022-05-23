c1f9418b14e22cf059f155c73e29517a397fc155
nbeckles <nbeckles@gmail.com>
2022-05-23 11:18:16 -0400

    add component for stock stats

--- src/Components/StockStats.js
+++ src/Components/StockStats.js
@@ -1,4 +1,20 @@
-const StockStats = () => {
+const StockStats = ({ dayHigh, dayLow, yearHigh, yearLow, marketCap, volume, peRatio, primaryExchange, aboutCompany }) => {
+
+    return (
+        <>
+            <p>
+                <span class="label">Day range:</span> ${dayLow} - ${dayHigh}
+            </p>
+            <p>
+                <span class="label">Year range:</span> ${yearLow} - ${yearHigh}
+            </p>
+            <p><span class="label">Market cap:</span>{marketCap}</p>
+            <p><span className="label">Volume:</span>{volume}</p>
+            <p><span className="label">P/E Ratio:</span>{peRatio}</p>
+            <p><span className="label">Primary Exchange</span>{primaryExchange}</p>
+            <p><span className="label">About</span>{aboutCompany}</p>
+        </>
+    );
 
 }
 
