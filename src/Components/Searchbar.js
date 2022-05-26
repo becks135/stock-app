2fd0c8af7f1f83305c5d7f135e7879ec9ae77096
nbeckles <nbeckles@gmail.com>
2022-05-26 17:38:27 -0400

    fix form submit for searchbar

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -3,11 +3,10 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = ({placeholderText, inputId, handleSubmit}) => {
     return(
-        <form className="search-bar">
+        <form className="search-bar" onSubmit={e=>handleSubmit(e)}>
             <label htmlFor={inputId} className="sr-only">{placeholderText}</label>
             <input type="text" name={inputId} id={inputId} placeholder={placeholderText}/>
-            <button 
-                onClick={e=>handleSubmit(e)}>
+            <button type="submit">
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
         </form>
